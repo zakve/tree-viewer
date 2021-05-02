ba8fd487c7f3d1798bd990aa607aa85b6ee783c5
zakve <zaklas.martin@gmail.com>
2021-05-02 11:02:11 +0200

    Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <Container maxWidth="sm">
+          <Typography variant='h3'>Tree Viewer</Typography>
+          Container
+        </Container>
+      </main>
     </div>
   );
 }
